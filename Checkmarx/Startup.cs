314bfe6043f0c112a353616ffafcee87f96ac499
Mike Chen <mikexc@hotmail.com>
2016-12-29 00:05:16 -0500

    Add project files.

---
+++ Checkmarx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Checkmarx.Startup))]
+namespace Checkmarx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
