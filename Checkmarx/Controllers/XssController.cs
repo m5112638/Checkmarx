7c45d6eaf62809045f3d04fb22d4f4fb58848ac5
Mike Chen <mikexc@hotmail.com>
2017-01-03 22:41:40 -0500

    .

--- Checkmarx/Controllers/XssController.cs
+++ Checkmarx/Controllers/XssController.cs
@@ -8,8 +8,23 @@ namespace Checkmarx.Controllers
 {
     public class XssController : Controller
     {
-        // GET: Xss
+        // GET: Xss/Index
         public ActionResult Index(string s)
+        {
+            return View(new Models.XssViewModel() { Value = s });
+        }
+
+        // GET: Xss/Index2
+        [ValidateInput(false)]
+        public ActionResult Index2(string s)
+        {
+            return View("Index", new Models.XssViewModel() { Value = s });
+        }
+
+        // GET: Xss/XssA
+        [Authorize]
+        [ValidateInput(false)]
+        public ActionResult XssA(string s)
         {
             return View(new Models.XssViewModel() { Value = s });
         }
